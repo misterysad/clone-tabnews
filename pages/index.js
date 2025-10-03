9051d700b49e1e05cc9e5b0234c8328696a890f8
misterysad <106823187+misterysad@users.noreply.github.com>
2025-10-03 01:14:16 +0000

    Teste gif

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-function Home() {
-    return <h1>Eu te amo muito Belinhaaa! ❤ </h1>
-    
+export default function Home(){
+
+return (
+    <main style = {{textAlign:"center", marginTop: "50px"}}>
+<h1>Eu te amo muito meu amor 💕</h1>
+<img
+src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2szd3hzdmQxbDN5YWp4MGh1cDF1MjhwbG05a3VndHdtaHJxem15byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1JmGiBtqTuehfYxuy9/giphy.gif"
+width = {300}
+        />
+    </main>
+    );
 }
-
-
-export default Home;
